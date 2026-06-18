2588fe15bc23215e6b76a08f36b3ec74436f7b6f
Vaibhav Paliwal <vaibahvpaliwal1702@gmail.com>
2026-06-18 08:55:33 +0530

    server file fix

--- backend/server.js
+++ backend/server.js
@@ -117,4 +117,5 @@ app.get('/events',(req,res) => {
     res.json(data.events);
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
